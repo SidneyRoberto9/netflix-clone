7f6235776e0ddfdf33e79978971aeaaa70fe8915
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-04-25 03:51:06 -0300

    ✨ feat: stylized login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -17,7 +17,7 @@ export default function Login() {
   };
 
   return (
-    <div className="register">
+    <div className="login">
       <div className="top">
         <div className="wrapper">
           <img
@@ -29,27 +29,19 @@ export default function Login() {
         </div>
 
         <div className="container">
-          <h1>Unlimited movies, TV shows, and more.</h1>
-          <h2>Watch anywhere. Cancel anytime.</h2>
-          <p>
-            Ready to watch? Enter your email to create or restart your
-            membership.
-          </p>
-          {!email ? (
-            <div className="input">
-              <input type="email" placeholder="email address" ref={emailRef} />
-              <button className="registerButton" onClick={handleStart}>
-                Get Started
-              </button>
-            </div>
-          ) : (
-            <div className="input">
-              <input type="password" placeholder="password" ref={passwordRef} />
-              <button className="registerButton" onClick={handleFinish}>
-                Start
-              </button>
-            </div>
-          )}
+          <form>
+            <h1>Sign In</h1>
+            <input type="email" placeholder="Email or phone number" />
+            <input type="password" placeholder="Password" />
+            <button className="loginButton">Sign In</button>
+            <span>
+              New to Netflix? <b>Sign up now.</b>
+            </span>
+            <small>
+              This page is protected by Google reCAPTCHA to ensure you're not a
+              bot. <b>Learn more</b>.
+            </small>
+          </form>
         </div>
       </div>
     </div>
