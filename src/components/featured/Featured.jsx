5ceadd2873d9d6e2e158fe1fa225ea6f93295fc2
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-04-25 01:11:01 -0300

    ✨ feat: create featured component and use

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,15 @@
+import "./featured.scss";
+import React from "react";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <img
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Featured;
