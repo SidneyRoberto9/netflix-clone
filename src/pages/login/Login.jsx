1442cb9e65aa38a0e335d0a783cc6a97cecccceb
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-04-25 03:38:25 -0300

    🐛 fix: Login className

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import "./login.scss";
 
-export default function Register() {
+export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
