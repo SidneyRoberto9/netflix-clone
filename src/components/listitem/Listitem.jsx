452898cc13b906fe0d5a5b1d13f606786734480e
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-05-16 10:45:16 -0300

    📦 refactor: add birgtness to hover cards

--- src/components/listitem/Listitem.jsx
+++ src/components/listitem/Listitem.jsx
@@ -23,18 +23,10 @@ export default function ListItem({ index, item, openModal, setContent }) {
     getMovie();
   }, [item]);
 
-  const style = {
-    left: isHovered && index * 245 - 50 + index * 2.5,
-    marginRight: isHovered && 2 + index * 2.5,
-  };
-
   return (
     <>
       <div
         className="listItem"
-        style={style}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
         onClick={() => {
           openModal(true);
           setContent(movie);
